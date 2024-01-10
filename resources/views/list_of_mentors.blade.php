5c5ddacd54c924235927146f5645597dc29abc8d
happy gikundiro <gikundirohappy@gmail.com>
2024-01-10 12:02:09 +0200

    added icons on mentors informations

--- resources/views/list_of_mentors.blade.php
+++ resources/views/list_of_mentors.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.admin')
 @section('title')
 @section('content')
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Project with Icons</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-...." crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
 <div>
     <div>
     <div class="flex justify-end mt-10">
@@ -20,11 +28,13 @@
        @if($mentor->image_path)
     <img src="{{ asset('storage/' . $mentor->image_path) }}" alt="Mentor Image" class="object-cover w-[130px] h-[130px] mb-4 rounded-full">
        @endif
-
-        <h1 class="flex items-center justify-end font-semibold">{{ $mentor->mentor_name }}</h1>
-        </div>
-        <h2 class="font-bold text-turtle-green text-lg">{{ $mentor->field }}</h2>
-        <h3>{{ $mentor->location }}</h3>
+<div class="flex items-center">
+    <div class="space-y-3">
+        <h1 class="text-lg">{{ $mentor->mentor_name }}</h1>
+        
+        <h2 class="text-turtle-green text-md"><span class="mr-3 text-black"><i class="fa-solid fa-layer-group"></i></span>{{ $mentor->field }}</h2>
+        <h3><span class="mr-3"><i class="fa-solid fa-location-dot"></i></span>{{ $mentor->location }}</h3></div></div>
+    </div>
         <p class="leading-6">{{ $mentor->description }}</p>
        </div>
          @endforeach
@@ -33,4 +43,6 @@
 
     </div>
 </div>
+</body>
+</html>
 @endsection
